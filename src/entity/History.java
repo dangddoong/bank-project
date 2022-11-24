5ca3ff5c9825c6a9441935627db98282fe7917d6
Dong Kyun <laancer4@gmail.com>
2022-11-24 20:52:30 +0900

    restart

--- src/entity/History.java
+++ src/entity/History.java
@@ -1,46 +1,50 @@
 package entity;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class History {
     //거래 일자랑 시간을 date로 성형해서 찍기
-    private Date tradeDate;
-    private Date tradeTime;
+    private LocalDate tradeDate;
+    private LocalTime tradeTime;
     //AccountNum 키값
-    private int accountNum;
+    private String accountNum;
     //입급 출급 여부 - 판별값 보내서 화인
-    private int isTraded;
+    private String isTraded;
     //유저가 넣어준 금액
-    private int money;
+    private String money;
     //은행명 //받아서 쓰는걸로 할 거 같네요 - 고정할 것이냐 받아올 것이냐
     private String bankName;
 
-    public History(int accountNum, int isTraded, int money, String bankName) {
-        this.tradeDate = new Date("yyyy, MM, dd");
-        this.tradeTime = new Date("hh : mm");
+    //format으로 LocalDateTime 바꿔야 합니다.
+
+    public History(String accountNum, String isTraded, String money, String bankName) {
+        this.tradeDate = LocalDate.now();
+        this.tradeTime = LocalTime.now();
         this.accountNum = accountNum;
         this.isTraded = isTraded;
         this.money = money;
         this.bankName = bankName;
     }
 
-    public Date getTradeDate() {
+    public LocalDate getTradeDate() {
         return tradeDate;
     }
 
-    public Date getTradeTime() {
+    public LocalTime getTradeTime() {
         return tradeTime;
     }
 
-    public int getAccountNum() {
+    public String getAccountNum() {
         return accountNum;
     }
 
-    public int getIsTraded() {
+    public String getIsTraded() {
         return isTraded;
     }
 
-    public int getMoney() {
+    public String getMoney() {
         return money;
     }
 
