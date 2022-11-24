0db6371001d7ed19bc9cea5432cf2efc8844fb7b
yichi <coding2210@gmail.com>
2022-11-24 13:14:50 +0900

    Update AdminLogic.java
    무조건 0번인 Admin 계정은 for문 돌릴 필요 없이 그냥 if로 판단하는 로그인로직 구현

--- src/application/AdminLogic.java
+++ src/application/AdminLogic.java
@@ -1,4 +1,18 @@
 package application;
 
+import data.UserDB;
+import entity.User;
 public class AdminLogic {
+    public boolean login(String id, String pw) {
+       UserDB userDB = new UserDB();
+        if(userDB.getAdminid() != id){
+            throw new IllegalArgumentException("관리자가 아닙니다.");
+        }
+
+        if(UserDB.getAdminpw() != pw){
+            throw new IllegalArgumentException("비밀번호가 틀렸습니다. 다시 입력해수제요");
+        }
+            return true;
+        }
+
 }
