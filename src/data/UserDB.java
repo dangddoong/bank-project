77d00fa7551573dd56cf40bd6644515c30ddb3a0
Gwanho KIM <bigquan977@gmail.com>
2022-11-26 00:33:54 +0900

    remove UserDB unused Methods

--- src/data/UserDB.java
+++ src/data/UserDB.java
@@ -6,23 +6,16 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 public class UserDB {
-
-    private static UserDB userDB = new UserDB();
+    private static final UserDB userDB = new UserDB();
 
     public static UserDB getInstance() {
         return userDB;
     }
 
-    private ArrayList<User> userList = new ArrayList<>();
+    private final ArrayList<User> userList = new ArrayList<>();
 
-    // 유저 등록( DB에 넣기)
     public void insertUser(User user){
         userList.add(user);
-    }
-
-    //모든 유저 조회(은행은 모든 계좌의 목록을 조회할 수 있다... 를 구현하기 위해서)
-    public ArrayList<User> getAllUser(){
-        return userList;
     }
 
     public Optional<User> getUserByUserId(String userId) {
@@ -35,6 +28,13 @@ public class UserDB {
         }
         return Optional.ofNullable(findUser);
     }
+}
+
+/*
+    //모든 유저 조회(은행은 모든 계좌의 목록을 조회할 수 있다... 를 구현하기 위해서)
+    public ArrayList<User> getAllUser(){
+        return userList;
+    }
 
     //유저 1명 조회 - 소유자 명으로 찾기 (개인별 서비스용 - 수정/삭제/거래내역 조회 등)
     public User getUserByUserName(String userName){
@@ -45,4 +45,5 @@ public class UserDB {
         }
         return null;
     }
-}
+
+ */
