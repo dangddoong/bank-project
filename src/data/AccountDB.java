41198bfbe81a03cff9693596ccb99673d82ac48b
Gwanho KIM <bigquan977@gmail.com>
2022-11-26 00:34:33 +0900

    remove AccountDB unused Methods

--- src/data/AccountDB.java
+++ src/data/AccountDB.java
@@ -6,27 +6,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class AccountDB {
-
-    private static AccountDB accountDB = new AccountDB();
+    private static final AccountDB accountDB = new AccountDB();
 
     public static AccountDB getInstance() {
         return accountDB;
     }
 
-
-//    private int maxAccountID = 0;
-    //account가 있을때마다 max 를 하나씩 증가시키는 부분.
-    // 만약에 admin 에서 계좌 개수를 카운트하기로 한다면? 그때 구현할게요. 어렵진 않은데 충돌방지로 일단 킵...
-
-    // admin 계좌처리는 머리가 도저히 안돌아가서 내일 좀 하겠습니다.
-
-    private ArrayList<Account> accountList = new ArrayList<>();
+    private final ArrayList<Account> accountList = new ArrayList<>();
 
     public void insertAccount(Account account){
         accountList.add(account);
     }
 
-    // 계좌 삭제
     public void deleteAccount(Account account){
         accountList.remove(account);
     }
@@ -34,6 +25,19 @@ public class AccountDB {
     public ArrayList<Account> getAllAccount(){
         return accountList;
     }
+
+    public List<Account> getAllAccountByUserName(String userName) {
+        return accountList.stream().filter(x -> x.getUserName().equals(userName)).collect(Collectors.toList());
+    }
+}
+
+//    private int maxAccountID = 0;
+//account가 있을때마다 max 를 하나씩 증가시키는 부분.
+// 만약에 admin 에서 계좌 개수를 카운트하기로 한다면? 그때 구현할게요. 어렵진 않은데 충돌방지로 일단 킵...
+
+// admin 계좌처리는 머리가 도저히 안돌아가서 내일 좀 하겠습니다.
+
+/*
 
     //유저 1명 조회 - 계좌번호로 찾기 (개인별 서비스용 - 수정/삭제/거래내역 조회 등)
     public Account getAccountByAccountNum(String accountNum){
@@ -55,13 +59,5 @@ public class AccountDB {
         return null;
     }
 
-    public List<Account> getAllAccountByUserName(String userName) {
-        List<Account> accounts = accountList.stream().filter(x -> x.getUserName().equals(userName)).collect(Collectors.toList());
-        return accounts;
-    }
 
-//test
-
-
-
-}
+ */
